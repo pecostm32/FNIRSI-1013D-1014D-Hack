d50dfa45a90e2678eb46ecac14f9ebe68ce664e0
pecostm32 <53696654+pecostm32@users.noreply.github.com>
2021-07-08 21:53:48 +0200

    Project for extracting bitmaps from the scope binary

---
+++ Test code/fnirsi_bitmap_converter/convert_scope_bitmap.c
@@ -0,0 +1,233 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+  //Read binary image data xsize = 298, ysize = 98
+  //16 bits per pixel
+  //5 bits R, 6 bits G, 5 bits B
+  
+  unsigned char  bitmap[800000];
+  unsigned short image[200000];
+  unsigned int   length;
+  
+  unsigned int    pixel;
+  unsigned int    pixels;
+  unsigned int    row;
+  unsigned int    rows;
+  unsigned int    bytesperrow;
+  int             vertres;
+  int             horzres;
+  unsigned int    size;
+  unsigned int    filesize;
+  unsigned char   buffer[54];
+  unsigned short *dptr;
+  unsigned char  *sptr;
+  
+  unsigned int address;
+
+  FILE *fi = fopen("W25Q32_scope.bin", "rb");
+  
+//Still to do
+//8004301c
+//80041654
+  
+  
+//#define AddressOfBitmap   0x80047d74    //SAVE PIC
+//#define AddressOfBitmap   0x8004973c    //SAVE WAVE
+//#define AddressOfBitmap   0x800449e4    //CTRL
+//#define AddressOfBitmap   0x800463ac    //MEAS URES
+//#define NofPixels         60
+//#define NofLines          55
+
+//#define AddressOfBitmap   0x8004B104  //V+ V-
+//#define NofPixels         60
+//#define NofLines          176
+    
+//#define AddressOfBitmap   0x80050384    //menu button
+//#define NofPixels           80
+//#define NofLines            42
+  
+//#define AddressOfBitmap   0x80051fa4  //Channel menu
+//#define NofPixels         183
+//#define NofLines          252
+
+//#define AddressOfBitmap   0x800687ec  //trigger menu
+//#define NofPixels         172
+//#define NofLines          186
+
+//#define AddressOfBitmap   0x800781DC  //meas ures menu
+//#define NofPixels         499
+//#define NofLines          214
+  
+//#define AddressOfBitmap   0x800AC420   //Main menu
+//#define NofPixels         149
+//#define NofLines          235
+  
+//#define AddressOfBitmap   0x800BD5AE    //Start calibration text
+//#define NofPixels         197
+//#define NofLines          60
+  
+//#define AddressOfBitmap   0x800C3206     //Calibrating... text
+//#define AddressOfBitmap   0x800C6596       //Calibration successful text
+//#define NofPixels         110
+//#define NofLines          60
+  
+//#define AddressOfBitmap   0x800c9926     //System settings
+//#define NofPixels         244
+//#define NofLines          294
+  
+#define AddressOfBitmap   0x800EC998
+#define NofPixels         244
+#define NofLines          294
+  
+  if(fi)
+  {
+    //Location of bitmap in file
+    address = (AddressOfBitmap & 0x1FFFFFFF) + 0x27020;
+    
+    //Point to it in the file
+    fseek(fi, address, SEEK_SET);
+    
+    size = NofPixels * NofLines;
+    
+    //Get the bitmap data
+    length = fread(&image, 2, size, fi);
+    
+    //Original bitmap parameters
+//    pixels = 298;
+//    rows = 98;
+
+    pixels = NofPixels;
+    rows = NofLines;
+    
+    //Number of bytes on a row for a 24 bits per pixel bitmap
+    bytesperrow = pixels * 3;
+    
+    //Adjust for 32 bit segments
+    bytesperrow = (bytesperrow + 3) & 0x0FFFFFFC;
+    
+    //Calculate the size of the bitmap in bytes
+    size = bytesperrow * rows;
+    
+    memset(bitmap, 0, sizeof(bitmap));
+    
+    dptr = image;
+
+    //Convert to 24 bits per pixel based on 565 RGB
+    for(row=0;row<rows;row++)
+    {
+       sptr = bitmap + ((rows-row) * bytesperrow);
+
+       for(pixel=0;pixel<pixels;pixel++)
+       {
+         *sptr++ = (unsigned char)(*dptr << 3) & 0x00F8;
+         *sptr++ = (unsigned char)(*dptr >> 3) & 0x00FC;
+         *sptr++ = (unsigned char)(*dptr >> 8) & 0x00F8;
+         
+         dptr++;
+       }
+    }
+
+    FILE *fo = fopen("scope_text_image.bmp", "wb");
+
+    if(fo)
+    {
+      //Bitmap magic number
+      buffer[0] = 'B';
+      buffer[1] = 'M';
+      
+      filesize = size + 54;
+      
+      //File size
+      buffer[2] = filesize & 0xFF;
+      buffer[3] = (filesize >>  8) & 0xFF;
+      buffer[4] = (filesize >> 16) & 0xFF;
+      buffer[5] = (filesize >> 24) & 0xFF;
+
+      //reserved
+      buffer[6] = 0;
+      buffer[7] = 0;
+      buffer[8] = 0;
+      buffer[9] = 0;
+      
+      //Bitmap offset
+      buffer[10] = 54;
+      buffer[11] = 0;
+      buffer[12] = 0;
+      buffer[13] = 0;
+
+      //Header size
+      buffer[14] = 40;
+      buffer[15] = 0;
+      buffer[16] = 0;
+      buffer[17] = 0;
+
+      //Bitmap width
+      buffer[18] = pixels & 0xFF;
+      buffer[19] = (pixels >>  8) & 0xFF;
+      buffer[20] = (pixels >> 16) & 0xFF;
+      buffer[21] = (pixels >> 24) & 0xFF;
+      
+      //Bitmap height
+      buffer[22] = rows & 0xFF;
+      buffer[23] = (rows >>  8) & 0xFF;
+      buffer[24] = (rows >> 16) & 0xFF;
+      buffer[25] = (rows >> 24) & 0xFF;
+
+      //Number of planes      
+      buffer[26] = 1;
+      buffer[27] = 0;
+      
+      //Bits per pixel
+      buffer[28] = 24;
+      buffer[29] = 0;
+
+      //Bitmap compression
+      buffer[30] = 0;
+      buffer[31] = 0;
+      buffer[32] = 0;
+      buffer[33] = 0;
+      
+      //Image size
+      buffer[34] = 0;
+      buffer[35] = 0;
+      buffer[36] = 0;
+      buffer[37] = 0;
+      
+      //Horiz res
+      buffer[38] = 0;
+      buffer[39] = 0;
+      buffer[40] = 0;
+      buffer[41] = 0;
+
+      //Vert res
+      buffer[42] = 0;
+      buffer[43] = 0;
+      buffer[44] = 0;
+      buffer[45] = 0;
+
+      //Nof colors
+      buffer[46] = 0;
+      buffer[47] = 0;
+      buffer[48] = 0;
+      buffer[49] = 0;
+      
+      //Colors important
+      buffer[50] = 0;
+      buffer[51] = 0;
+      buffer[52] = 0;
+      buffer[53] = 0;
+      
+      fwrite(buffer, 1, 54, fo);
+
+      fwrite(bitmap, 1, size, fo);
+
+      fclose(fo);
+    }
+
+    fclose(fi);
+  }  
+}
+
---
+++ Test code/fnirsi_bitmap_converter/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=fnirsi_bitmap_converter
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/fnirsi_bitmap_converter
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=fnirsibitmapconverter.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/fnirsibitmapconverter.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=fnirsi_bitmap_converter
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/fnirsi_bitmap_converter
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=fnirsibitmapconverter.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/fnirsibitmapconverter.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
