dd847dc71818d1def96df76c07d923b94e4a3906
pecostm32 <53696654+pecostm32@users.noreply.github.com>
2021-08-03 20:05:01 +0200

    My work files

--- Software reverse engineering/C analysis/Startup/main.c
+++ Software reverse engineering/C analysis/Startup/main.c
@@ -1,68 +1,77 @@
+//-----------------------------------------------------------------------------------------------
+
 void main(void)
 
 {
   int iVar1;
   undefined auStack568 [568];
   
-  sys_clock_init();
-  some_memory_stuff();
-  sys_init_uart0(BAUD_115200);
+  sys_clock_init();                         //Has been implemented
+  setup_mmu();                              //Only caches enabled
+  sys_init_uart0(BAUD_115200);              //Not needed because there is no free uart available
   setup_timer_int();
-  sys_spi0_init();
-  fpga_init();
-  turn_off_brightness();
-  sys_init_display();
-  FUN_8001931c();
-  tp_i2c_setup();
-  iVar1 = FUN_80033714(auStack568,&DAT_8003545c,1);
-  if (iVar1 != 0) {
+  sys_spi0_init();                          //Has been implemented
+  fpga_init();                              //Done
+  turn_off_brightness();                    //Also done
+  sys_init_display();                       //Finished
+  setup_display_lib();                      //Made my own and is working
+  tp_i2c_setup();                           //Up and running
+
+  //Needs to be done still, but is for a later moment in time
+  iVar1 = check_sd_card((uint)auStack568,(uint)&DAT_8003545c,1);
+
+  if (iVar1 != 0)
+  {
     *(undefined *)(DAT_80035460 + 2) = 100;
     fpga_write_cmd('8');
     fpga_write_data(-0x16);
-    fpga_write_data((uchar)DEF_BRIGHTNESS);
-    set_display_color(0);
-    display_clear_rect(0,0,800,0x1e0);
-    set_display_color(0xff0000);
-    FUN_80019730(PTR_PTR_FUN_80035468);
-    FUN_800197c8(0);
+    fpga_write_data((uchar)DEFAULT_BRIGHTNESS);
+    set_display_fg_color(0);
+    display_fill_rect(0,0,800,0x1e0);
+    set_display_fg_color(0xff0000);
+    select_font(PTR_FONT_3_80035468);
+    set_font_paint_mode(0);
     display_text(s_SD_ERROR_8003546c,0x1e,0x32);
-    do {
-                    // WARNING: Do nothing block with infinite loop
-    } while( true );
+
+    while( true );
   }
-  FUN_8001c138();
-  FUN_8002a17c();
-  FUN_80002b7c();
-  FUN_800232e4();
-  FUN_80016af4();
-  FUN_8000690c();
-  FUN_80009658();
-  FUN_80001314();
-  FUN_8000689c();
-  FUN_800095e8();
-  FUN_800068d4();
-  FUN_80009620();
-  FUN_80012a4c();
-  FUN_80024e18();
-  FUN_800266c4();
-  FUN_800267e8();
-  FUN_80002790();
-  FUN_80026808();
-  FUN_800267c4();
-  FUN_80026828();
-  FUN_8000696c();
-  FUN_800096b8();
-  FUN_80017ce0();
-  FUN_80026918();
-  FUN_8000a024();
-  FUN_8000e8f0();
-  FUN_8001d380();
-  do {
-    FUN_8000a54c();
-    FUN_800253e8();
-    thunk_FUN_8002b124();
-    FUN_8001a3b4();
+
+  check_hardware();                         //Hardware check only performed when flash address 0x001FE000 not contains 0x9086  
+  FUN_8002a17c();                           //Some usb setup stuff
+  setup_power_mon_int();
+  load_config_data();
+  check_sys_ok_load_config_on_fail();
+  set_fpga_channel1_enable();               //Implemented
+  set_fpga_channel2_enable();               //Implemented
+  FUN_80001314();                           //Some variable initialization. set_trigger_level_dividers. Hard coded in my version
+  set_fpga_channel1_voltperdiv();           //Implemented
+  set_fpga_channel2_voltperdiv();           //Implemented
+  set_fpga_channel1_coupling();             //Implemented
+  set_fpga_channel2_coupling();             //Implemented
+  FUN_80012a4c();                           //Implemented (fpga_enable_system)
+  init_parameter_conversion();              //Implemented and working
+  FUN_800266c4();                           //set_fpga_trigger_timebase
+  set_fpga_trigger_channel();               //Implemented
+  set_fpga_channel_trigger_channel();       //Implemented
+  set_fpga_trigger_edge();                  //Implemented
+  FUN_800267c4();                           //set_fpga_trigger_level  needs more research
+  set_fpga_trigger_mode();                  //Implemented
+  FUN_8000696c();                           //Channel 1 offset needs mre research
+  FUN_800096b8();                           //Channel 2 offset needs mre research
+  FUN_80017ce0();                           //Something with battery chagre level and FPGA command 0x3C. Implemented
+  FUN_80026918();                           //Another hardware check only performed when flash address 0x001FE000 not contains 0x9086
+  FUN_8000a024();                           //No idea about this one yet????
+  setup_main_screen();                      //Implemented
+  set_translated_brightness();              //Implemented
+
+  do
+  {
+    monitor_battery();
+    process_trace_data();
+    display_cursors();
+    touch_handler();                        //Mostly implemented
   } while( true );
 }
 
+//-----------------------------------------------------------------------------------------------
 
---
+++ Software reverse engineering/C analysis/Startup/reset_config_data.c
@@ -0,0 +1,207 @@
+//-----------------------------------------------------------------------------------------------
+
+void reset_config_data(void)
+
+{
+  undefined *puVar1;
+  undefined *puVar2;
+  int iVar3;
+  
+  puVar1 = DAT_80027eec;
+  *DAT_80027eec = 1;
+  puVar1[2] = 0;
+  puVar1[1] = 0;
+  puVar1[3] = 0;
+  puVar2 = DAT_80027ef0;
+  puVar1[4] = 0;
+  *(undefined2 *)(puVar1 + 6) = 300;
+  puVar1[0xc] = 1;
+  puVar1[0xe] = 0;
+  puVar1[0xd] = 0;
+  puVar1[0xf] = 0;
+  puVar1[0x10] = 0;
+  *(undefined2 *)(puVar1 + 0x12) = 100;
+  puVar1[0x21] = 0;
+  puVar1[0x22] = 0;
+  puVar1[0x23] = 0;
+  *(undefined2 *)(puVar1 + 0x26) = 200;
+  *(undefined2 *)(puVar1 + 0x24) = 0x16a;
+  puVar1[0x16] = 0;
+  puVar1[10] = 0x13;
+  puVar1[0x48] = 0;
+  *puVar2 = 5;
+  puVar2[0x142] = 1;
+  puVar2[1] = 4;
+  puVar2[0x132] = 1;
+  puVar2[2] = 7;
+  puVar2[0x162] = 1;
+  puVar2[3] = 0x11;
+  puVar2[0x202] = 1;
+  puVar2[4] = 0x13;
+  puVar2[0x232] = 1;
+  puVar2[0x100] = 0;
+  puVar2[0x112] = 0;
+  puVar2[0x122] = 0;
+  puVar2[0x152] = 0;
+  puVar2[0x172] = 0;
+  puVar2[0x182] = 0;
+  puVar2[0x192] = 0;
+  puVar2[0x1a2] = 0;
+  puVar2[0x1b2] = 0;
+  puVar2[0x1c2] = 0;
+  puVar2[0x1d2] = 0;
+  puVar2[0x1e2] = 0;
+  puVar2[0x1f2] = 0;
+  puVar2[0x212] = 0;
+  puVar2[0x242] = 0;
+  puVar2[0x252] = 0;
+  puVar2[0x262] = 0;
+  puVar2[0x272] = 0;
+  puVar2[0x282] = 0;
+  memclear((uint)(puVar2 + 5),0x13);
+  puVar2[0x292] = 0;
+  *(undefined2 *)(puVar2 + 0x294) = 0xb7;
+  *(undefined2 *)(puVar2 + 0x296) = 0x223;
+  puVar2[0x29a] = 0;
+  *(undefined2 *)(puVar2 + 0x29c) = 0xa7;
+  iVar3 = DAT_80027ef4;
+  *(undefined2 *)(puVar2 + 0x29e) = 0x148;
+  puVar1[0x42] = 0;
+  puVar1[0x18] = 1;
+  puVar1[0x17] = 1;
+  *(undefined *)(iVar3 + 2) = 100;
+  *(undefined *)(iVar3 + 3) = 0xf;
+  *(undefined *)(iVar3 + 4) = 1;
+  *(undefined *)(iVar3 + 7) = 0;
+  return;
+}
+
+//-----------------------------------------------------------------------------------------------
+
+void save_config_data(uint *address)
+
+{
+  byte *pbVar1;
+  int iVar2;
+  int iVar3;
+  undefined2 *puVar4;
+  uint *puVar5;
+  byte *pbVar6;
+  int iVar7;
+  
+  puVar4 = DAT_80027d84;
+  iVar3 = DAT_80027d80;
+  iVar2 = DAT_80027d7c;
+  pbVar1 = DAT_80027d78;
+  *(ushort *)address = (ushort)*DAT_80027d78;
+  puVar5 = address + 0x53;
+  pbVar6 = (byte *)(iVar3 + -1);
+  iVar7 = 0xc;
+  *(ushort *)((int)address + 2) = (ushort)pbVar1[3];
+  *(ushort *)(address + 1) = (ushort)pbVar1[4];
+  *(ushort *)((int)address + 6) = (ushort)pbVar1[1];
+  *(ushort *)(address + 2) = (ushort)pbVar1[2];
+  *(ushort *)((int)address + 10) = (ushort)pbVar1[0xc];
+  *(ushort *)(address + 3) = (ushort)pbVar1[0xf];
+  *(ushort *)((int)address + 0xe) = (ushort)pbVar1[0x10];
+  *(ushort *)(address + 4) = (ushort)pbVar1[0xd];
+  *(ushort *)((int)address + 0x12) = (ushort)pbVar1[0xe];
+  *(ushort *)(address + 5) = (ushort)pbVar1[10];
+  *(ushort *)((int)address + 0x16) = (ushort)pbVar1[0x16];
+  *(ushort *)(address + 6) = (ushort)pbVar1[0x21];
+  *(ushort *)((int)address + 0x1a) = (ushort)pbVar1[0x22];
+  *(ushort *)(address + 7) = (ushort)pbVar1[0x23];
+  *(ushort *)((int)address + 0x1e) = (ushort)pbVar1[0x42];
+  *(undefined2 *)(address + 0x14) = *(undefined2 *)(pbVar1 + 0x24);
+  *(undefined2 *)((int)address + 0x52) = *(undefined2 *)(pbVar1 + 0x26);
+  *(undefined2 *)(address + 0x15) = *(undefined2 *)(pbVar1 + 6);
+  *(undefined2 *)((int)address + 0x56) = *(undefined2 *)(pbVar1 + 0x12);
+  *(ushort *)(address + 0x1e) = (ushort)*(byte *)(iVar2 + 2);
+  *(ushort *)((int)address + 0x7a) = (ushort)*(byte *)(iVar2 + 3);
+  *(ushort *)(address + 0x1f) = (ushort)*(byte *)(iVar2 + 4);
+  *(ushort *)((int)address + 0x7e) = (ushort)*(byte *)(iVar2 + 7);
+  *(ushort *)(address + 0x28) = (ushort)*(byte *)(iVar3 + 0x100);
+  *(ushort *)((int)address + 0xa2) = (ushort)*(byte *)(iVar3 + 0x112);
+  *(ushort *)(address + 0x29) = (ushort)*(byte *)(iVar3 + 0x122);
+  *(ushort *)((int)address + 0xa6) = (ushort)*(byte *)(iVar3 + 0x132);
+  *(ushort *)(address + 0x2a) = (ushort)*(byte *)(iVar3 + 0x142);
+  *(ushort *)((int)address + 0xaa) = (ushort)*(byte *)(iVar3 + 0x152);
+  *(ushort *)(address + 0x2b) = (ushort)*(byte *)(iVar3 + 0x162);
+  *(ushort *)((int)address + 0xae) = (ushort)*(byte *)(iVar3 + 0x172);
+  *(ushort *)(address + 0x2c) = (ushort)*(byte *)(iVar3 + 0x182);
+  *(ushort *)((int)address + 0xb2) = (ushort)*(byte *)(iVar3 + 0x192);
+  *(ushort *)(address + 0x2d) = (ushort)*(byte *)(iVar3 + 0x1a2);
+  *(ushort *)((int)address + 0xb6) = (ushort)*(byte *)(iVar3 + 0x1b2);
+  *(ushort *)(address + 0x2e) = (ushort)*(byte *)(iVar3 + 0x1c2);
+  *(ushort *)((int)address + 0xba) = (ushort)*(byte *)(iVar3 + 0x1d2);
+  *(ushort *)(address + 0x2f) = (ushort)*(byte *)(iVar3 + 0x1e2);
+  *(ushort *)((int)address + 0xbe) = (ushort)*(byte *)(iVar3 + 0x1f2);
+  *(ushort *)(address + 0x30) = (ushort)*(byte *)(iVar3 + 0x202);
+  *(ushort *)((int)address + 0xc2) = (ushort)*(byte *)(iVar3 + 0x212);
+  *(ushort *)(address + 0x31) = (ushort)*(byte *)(iVar3 + 0x232);
+  *(ushort *)((int)address + 0xc6) = (ushort)*(byte *)(iVar3 + 0x242);
+  *(ushort *)(address + 0x32) = (ushort)*(byte *)(iVar3 + 0x252);
+  *(ushort *)((int)address + 0xca) = (ushort)*(byte *)(iVar3 + 0x262);
+  *(ushort *)(address + 0x33) = (ushort)*(byte *)(iVar3 + 0x272);
+  *(ushort *)((int)address + 0xce) = (ushort)*(byte *)(iVar3 + 0x282);
+  *(undefined2 *)(address + 0x3c) = *puVar4;
+  *(undefined2 *)((int)address + 0xf2) = puVar4[1];
+  *(undefined2 *)(address + 0x3d) = puVar4[2];
+  *(undefined2 *)((int)address + 0xf6) = puVar4[3];
+  *(undefined2 *)(address + 0x3e) = puVar4[4];
+  *(undefined2 *)((int)address + 0xfa) = puVar4[5];
+  *(undefined2 *)(address + 0x3f) = puVar4[6];
+  *(undefined2 *)((int)address + 0xfe) = puVar4[7];
+  *(undefined2 *)(address + 0x40) = puVar4[8];
+  *(undefined2 *)((int)address + 0x102) = puVar4[9];
+  *(undefined2 *)(address + 0x41) = puVar4[10];
+  *(undefined2 *)((int)address + 0x106) = puVar4[0xb];
+  *(undefined2 *)(address + 0x42) = puVar4[0xc];
+  *(undefined2 *)((int)address + 0x10a) = puVar4[0xd];
+  *(undefined2 *)(address + 0x43) = puVar4[0xe];
+  *(undefined2 *)((int)address + 0x10e) = puVar4[0xf];
+  *(undefined2 *)(address + 0x44) = puVar4[0x10];
+  *(undefined2 *)((int)address + 0x112) = puVar4[0x11];
+  *(ushort *)(address + 0x45) = (ushort)*(byte *)(puVar4 + 0x12);
+  *(ushort *)((int)address + 0x116) = (ushort)*(byte *)((int)puVar4 + 0x25);
+  *(ushort *)(address + 0x46) = (ushort)*(byte *)(puVar4 + 0x13);
+  *(ushort *)((int)address + 0x11a) = (ushort)*(byte *)((int)puVar4 + 0x27);
+  *(ushort *)(address + 0x47) = (ushort)*(byte *)(puVar4 + 0x14);
+  *(ushort *)((int)address + 0x11e) = (ushort)*(byte *)((int)puVar4 + 0x29);
+  *(undefined2 *)(address + 0x48) = puVar4[0x15];
+  *(undefined2 *)((int)address + 0x122) = puVar4[0x16];
+  *(undefined2 *)(address + 0x49) = puVar4[0x17];
+  *(undefined2 *)((int)address + 0x126) = puVar4[0x18];
+  *(undefined2 *)(address + 0x4a) = puVar4[0x19];
+  *(undefined2 *)((int)address + 0x12a) = puVar4[0x1a];
+  *(ushort *)(address + 0x4b) = (ushort)*(byte *)(puVar4 + 0x1b);
+  *(ushort *)((int)address + 0x12e) = (ushort)*(byte *)((int)puVar4 + 0x37);
+  *(ushort *)(address + 0x4c) = (ushort)*(byte *)(puVar4 + 0x1c);
+  *(ushort *)((int)address + 0x132) = (ushort)*(byte *)((int)puVar4 + 0x39);
+  *(ushort *)(address + 0x4d) = (ushort)*(byte *)(puVar4 + 0x1d);
+  *(ushort *)((int)address + 0x136) = (ushort)*(byte *)((int)puVar4 + 0x3b);
+  *(undefined2 *)(address + 0x4e) = puVar4[0x1e];
+  *(undefined2 *)((int)address + 0x13a) = puVar4[0x1f];
+  *(undefined2 *)(address + 0x4f) = puVar4[0x20];
+  *(undefined2 *)((int)address + 0x13e) = puVar4[0x21];
+  *(undefined2 *)(address + 0x50) = puVar4[0x22];
+  *(undefined2 *)((int)address + 0x142) = puVar4[0x23];
+  *(ushort *)((int)address + 0x142) = (ushort)*(byte *)(iVar3 + 0x292);
+  *(undefined2 *)(address + 0x51) = *(undefined2 *)(iVar3 + 0x294);
+  *(undefined2 *)((int)address + 0x146) = *(undefined2 *)(iVar3 + 0x296);
+  *(ushort *)(address + 0x52) = (ushort)*(byte *)(iVar3 + 0x29a);
+  *(undefined2 *)((int)address + 0x14a) = *(undefined2 *)(iVar3 + 0x29c);
+  *(undefined2 *)(address + 0x53) = *(undefined2 *)(iVar3 + 0x29e);
+  do {
+    iVar7 = iVar7 + -1;
+    *(ushort *)((int)puVar5 + 2) = (ushort)pbVar6[1];
+    pbVar6 = pbVar6 + 2;
+    puVar5 = puVar5 + 1;
+    *(ushort *)puVar5 = (ushort)*pbVar6;
+  } while (iVar7 != 0);
+  *(undefined2 *)(address + 100) = *(undefined2 *)(pbVar1 + 0x44);
+  return;
+}
+
+//-----------------------------------------------------------------------------------------------
+
